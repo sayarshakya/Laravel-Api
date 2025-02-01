75c863e0d20121bd30ae802838422676d5fb8ef4
sayarshakya <sakyashire@hotmail.com>
2025-02-01 15:59:55 +0545

    new file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,3 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+    $app->middleware([
+        \App\Middleware\CorsMiddleware::class,
+    ]);
+    
